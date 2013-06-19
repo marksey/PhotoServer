65b5d304cc69f36e0c917ce239da83865f04ad4f
Ryan <you@example.com>
2013-06-18 21:15:44 -0700

    rest of files + label2 -- ryan

---
+++ RYAN/TestProjects/RyanMarkRepoTest1/RyanMarkRepoTest1/RyanMarkRepoTest1Tests/RyanMarkRepoTest1Tests.m
@@ -0,0 +1,32 @@
+//
+//  RyanMarkRepoTest1Tests.m
+//  RyanMarkRepoTest1Tests
+//
+//  Created by Rohit on 6/18/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "RyanMarkRepoTest1Tests.h"
+
+@implementation RyanMarkRepoTest1Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in RyanMarkRepoTest1Tests");
+}
+
+@end
